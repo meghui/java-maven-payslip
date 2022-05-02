308c99205d30d9410c5cfc42639e30292d5eb709
meghui <64728803+meghui@users.noreply.github.com>
2022-05-03 01:01:33 +1000

    Update Test.java
    Remove "else" to make  code more readable

--- src/main/java/com/example/Test.java
+++ src/main/java/com/example/Test.java
@@ -18,30 +18,36 @@ public class Test {
         System.out.print("Please input the csv for employee 1:");
         Scanner sc = new Scanner(System.in);
         String csvLine = sc.nextLine();
+        
         //emp1Input = "Monica,Tan,60050,9%,1 March";
         if (csvLine.trim().isEmpty()) {
             message = tagA + "No input." + tagB;
-        }else if (csvLine.trim().split(",").length < 5) {
+            break;
+        }
+        
+        if (csvLine.trim().split(",").length < 5) {
             message = "Invalid csv record. Valid csv record has 5 items separated by comma";
-        } else if (csvLine.trim().split(",").length > 5){
+            break;
+        }
+        
+        if (csvLine.trim().split(",").length > 5) {
             message = "Fields are over 5. Please make sure you input 5 fields only";
-        } else {
-            Validator v = new Validator();
-            message = v.checkMissing(csvLine.trim());
+            break;
+        } 
+        
+        Validator v = new Validator();
+        message = v.checkMissing(csvLine.trim());
+        if (message.equals("")) {
+            Parser parser = new Parser();
+            CSV csv = parser.parseCSV(csvLine);
+            message = v.checkIllegals(csv);
             if (message.equals("")) {
-                Parser parser = new Parser();
-                CSV csv = parser.parseCSV(csvLine);
-                message = v.checkIllegals(csv);
-                if (message.equals("")) {
-                    CSV csvFormatted = parser.parseCSVFormatted(csv);
-                    Employee employee = parser.parseEmployee(csvFormatted);
-                    PaySlip payslip = parser.parsePaySlip(employee);
-                    Printer printer = new Printer();
-                    message = printer.print(payslip);
-                }
+                CSV csvFormatted = parser.parseCSVFormatted(csv);
+                Employee employee = parser.parseEmployee(csvFormatted);
+                PaySlip payslip = parser.parsePaySlip(employee);
+                Printer printer = new Printer();
+                message = printer.print(payslip);
             }
         }
-        System.out.println(message);
-
     }
 }
